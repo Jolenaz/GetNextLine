9b10717fc22a28a139381715d0e2900dc85db203
Jonas BELLESSA <jbelless@e3r11p5.42.fr>
2015-12-03 16:07:00 +0100

    creation du header

--- get_next_line.c
+++ get_next_line.c
@@ -6,17 +6,26 @@
 /*   By: jbelless <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/03 12:44:26 by jbelless          #+#    #+#             */
-/*   Updated: 2015/12/03 12:53:34 by jbelless         ###   ########.fr       */
+/*   Updated: 2015/12/03 16:05:22 by jbelless         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "get_next_line.h"
+
+void ft_initbuffer(char **ptb, int buf_size)
+{
+	int i;
+
+	i = 0;
+	while (i < buf_size)
+	{
+		*ptb[i] = 0;
+		i++;
+	}
+}
 
 int	get_next_line(const int fd, char **line)
 {
-	int ret;
-
-	ret = 0;
 
 	return (ret);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,19 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   jbmaintest_GNT.c                                   :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jbelless <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/12/03 12:48:29 by jbelless          #+#    #+#             */
-/*   Updated: 2015/12/03 15:00:24 by jbelless         ###   ########.fr       */
+/*   Created: 2015/12/03 15:59:48 by jbelless          #+#    #+#             */
+/*   Updated: 2015/12/03 16:05:52 by jbelless         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-int main()
-{
+# include "libft.h"
+# include <sys/types.h>
+# include <sys/uio.h>
+:ew
+# define BUFF_SIZE 32
 
-	return (0);
-}
+
+#endif
