0ad43c9ac45522481f48cf58d63778169cf99873
Jonas BELLESSA <jbelless@e3r12p7.42.fr>
2015-12-18 15:48:51 +0100

    gnl avec bonus norme mais 32o de leaksgit add get_next_line.cgit add get_next_line.cadd

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jbelless <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/03 12:44:26 by jbelless          #+#    #+#             */
-/*   Updated: 2015/12/18 14:34:17 by jbelless         ###   ########.fr       */
+/*   Updated: 2015/12/18 15:48:18 by jbelless         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,49 +15,68 @@
 
 static int	ft_cop(char **str1, char **line, char **str2)
 {
+	char **vid;
+
+	vid = (char**)malloc(sizeof(char*));
+	*vid = NULL;
 	*(ft_strchr(*str1, '\n')) = '\0';
-	if ((*line = ft_strjoin(*line, *str1)) == NULL)
+	if ((*vid = ft_strjoin(*line, *str1)) == NULL)
 		return (-1);
+	ft_strdel(line);
+	*line = *vid;
 	free(*str2);
 	if ((*str2 = ft_strdup(ft_strchr(*str1, '\0') + 1)) == NULL)
 		return (-1);
 	return (1);
 }
 
-static int	read_get_next_line(const int fd, char **line, char **rest)
+static int	norme(const int fd, char **line, char **rest, int *tete)
 {
-	char		*buff;
-	int			tete;
 	char		**vid;
+	char		*buff;
 
-	buff = NULL;
-	vid = (char**)malloc(sizeof(char*));
-	*vid = NULL;
 	if ((buff = ft_strnew(BUFF_SIZE + 1)) == NULL)
 		return (-1);
+	vid = (char**)malloc(sizeof(char*));
+	*vid = NULL;
+	while ((*tete = read(fd, buff, BUFF_SIZE)) && *tete != -1)
+	{
+		if (*tete <= BUFF_SIZE)
+			buff[*tete] = 0;
+		if (ft_strchr(buff, '\n') != NULL)
+			return (ft_cop(&buff, line, rest));
+		else
+		{
+			if ((*vid = ft_strjoin(*line, buff)) == NULL)
+				return (-1);
+			ft_strdel(line);
+			*line = *vid;
+		}
+	}
+	ft_strdel(&buff);
+	return (*tete);
+}
+
+static int	read_get_next_line(const int fd, char **line, char **rest)
+{
+	int			tete;
+
 	if (*rest == NULL)
 		*rest = ft_strnew(BUFF_SIZE);
 	else if (ft_strchr(*rest, '\n') != NULL)
 		return (ft_cop(rest, line, rest));
 	else if ((*line = ft_strjoin(*line, *rest)) == NULL)
 		return (-1);
-	while ((tete = read(fd, buff, BUFF_SIZE)) && tete != -1)
-	{
-
-		if (tete <= BUFF_SIZE)
-			buff[tete] = 0;
-		if (ft_strchr(buff, '\n') != NULL)
-			return (ft_cop(&buff, line, rest));
-		else
-		{
-			if ((*vid = ft_strjoin(*line, buff)) == NULL)
-				return(-1);
-			ft_strdel(line);
-			*line = *vid;
-		}
-	}
-	ft_strdel(&buff);
+	if (norme(fd, line, rest, &tete) == 1)
+		return (1);
 	return (tete);
+}
+
+static void	init_doc(int fd, int *filed, t_doc **next, char **rest)
+{
+	*filed = fd;
+	*next = NULL;
+	*rest = NULL;
 }
 
 int			get_next_line(const int fd, char **line)
@@ -72,9 +91,7 @@ int			get_next_line(const int fd, char **line)
 	tmp = &doc;
 	if (tmp->rest == NULL)
 	{
-		doc.filed = fd;
-		doc.next = NULL;
-		doc.rest = NULL;
+		init_doc(fd, &(doc.filed), &(doc.next), &(doc.rest));
 		return (read_get_next_line(fd, line, &(doc.rest)));
 	}
 	while (tmp->filed != fd && tmp->next != NULL)
@@ -83,9 +100,8 @@ int			get_next_line(const int fd, char **line)
 	{
 		if ((tmp->next = (t_doc*)malloc(sizeof(t_doc))) == NULL)
 			return (-1);
-		tmp->next->filed = fd;
-		tmp->next->next = NULL;
-		tmp->next->rest = NULL;
+		init_doc(fd, &(tmp->next->filed),
+				&(tmp->next->next), &(tmp->next->rest));
 		return (read_get_next_line(fd, line, &tmp->next->rest));
 	}
 	return (read_get_next_line(fd, line, &tmp->rest));
